3830039d4d01425cd55a178be42bbeeb7069c356
perryjhonz <c23daniel.perry@usafa.edu>
2021-02-02 23:12:34 -0700

    first commit

---
+++ pex2sorts.c
@@ -0,0 +1,8 @@
+/** pex2sorts.c
+ * ===========================================================
+ * Name: Daniel Perry
+ * Section: T5
+ * Project: PEX2
+ * Purpose: Sorting algoritms
+ * Documentation:
+ * =========================================================== */
---
+++ pex2sorts.h
@@ -0,0 +1,8 @@
+/** pex2sorts.h
+ * ===========================================================
+ * Name: Daniel Perry
+ * Section: T5
+ * Project: PEX2
+ * Purpose: Sorting algoritms
+ * Documentation:
+ * =========================================================== */
